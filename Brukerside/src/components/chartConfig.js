cc0136956bc8afa66d281d82c54b541130db853f
ErikOkau <erikmsorheim@yahoo.no>
2023-05-24 15:47:11 +0200

    ee

--- Brukerside/src/components/chartConfig.js
+++ Brukerside/src/components/chartConfig.js
@@ -1,25 +1,46 @@
 import { collection, getCountFromServer, query, where } from 'firebase/firestore'
-import { auth, db  } from '../firebase/firebase.js'
+import { db, auth  } from '../firebase/firebase.js'
+import { onAuthStateChanged } from 'firebase/auth'
+import { ref } from 'vue'
 
 const collectionRef = collection(db, 'Kjøretimer')
-
-const completedRef = query(
-  collectionRef, 
-  where(
-    'Status', 
-    '==', 
-    'Completed'
-    ),
-  )
-const countComplete = await getCountFromServer(completedRef)
+const completeCount = ref(0)
+const countOngoing = ref(0)
 
 
-const ongoingRef = query(
-  collectionRef, 
-  where('Status', '==', 'Ongoing'),
-  )
-const countOngoing = await getCountFromServer(ongoingRef)
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    console.log("User is signed in")
 
+    console.log(auth.currentUser.uid)
+
+    const completedRef = query(
+      collectionRef, 
+      where('Status', '==', 'Completed'),
+      where('elevId','==', auth.currentUser.uid)
+    )
+    const completeCountData = await getCountFromServer(completedRef)
+
+    completeCount.value = completeCountData.data().count
+
+
+    
+    const ongoingRef = query(
+      collectionRef, 
+      where('Status', '==', 'Ongoing'),
+      where('elevId','==',auth.currentUser.uid)
+    )
+
+    const countOngoingData = await getCountFromServer(ongoingRef)
+
+    countOngoing.value = countOngoingData.data().count
+
+    console.log(completeCount.value)
+    console.log(countOngoing.value)
+  } else {
+    console.log("No user is signed in")
+  }
+})
 
 
 export const data = {
@@ -28,12 +49,12 @@ export const data = {
     {
       label: 'Fullført',
       backgroundColor: 'green',
-      data: [countComplete.data().count],
+      data: [completeCount],
     },
     {
       label: 'Fremtidig',
       backgroundColor: 'red',
-      data: [countOngoing.data().count],
+      data: [countOngoing],
     },
   ],
 }
--- Website/src/views/Hjem.vue
+++ Website/src/views/Hjem.vue
@@ -399,3 +399,4 @@ main {
 
 
 </style>
+vue
